545e355585bbeb1aa9f93db9667f7126c26d56ed
Sam <klioop2@gmail.com>
2021-11-26 20:19:23 +0900

    Create BookResponse file

---
+++ ReportFormat/Model/BookResponse.swift
@@ -0,0 +1,22 @@
+//
+//  BookResponse.swift
+//  ReportFormat
+//
+//  Created by klioop on 2021/11/26.
+//
+
+import Foundation
+
+struct BookResponse: Codable {
+    let items: [Item]
+    
+    struct Item: Codable {
+        let title: String
+        let image: String
+        let pubdate: String
+        let publisher: String
+    }
+    
+}
+
+typealias BookApiResponse = [BookResponse.Item]
